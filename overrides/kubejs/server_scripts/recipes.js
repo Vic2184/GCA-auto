03317712b42b9746a22ba5c9a194f4b400fccfe8
Vic2184 <17078461+vic2184@user.noreply.gitee.com>
2026-05-18 15:16:01 +0000

    1
    Signed-off-by: Vic2184 <17078461+vic2184@user.noreply.gitee.com>

---
+++ overrides/kubejs/server_scripts/recipes.js
@@ -0,0 +1,59 @@
+ServerEvents.recipes(event => {
+//create语法仅在安装kubejs-create后生效，请使用自定义方式
+  event.custom({
+  type: "create:crushing",
+  ingredients: [
+    {
+      item: "minecraft:flint"
+    }
+  ],
+  processing_time: 50,
+  results: [
+    {
+      id: "minecraft:gunpowder"
+    },
+  ]
+}).id("create:crushing/gunpowder_from_flint")
+//添加机械动力火药配方
+
+event.custom({
+  type: "create:compacting",
+  heat_requirement:"heated",
+  ingredients: [
+    {
+      tag: "c:ingots/copper"
+    },
+    {
+      tag:"c:ingots/iron"
+    }
+  ],
+  results: [
+    {
+      id: "pointblank:gunmetal_ingot"
+    }
+  ]
+}).id("create:compacting/gunmetal_heated")
+event.custom({
+  type: "create:compacting",
+  ingredients: [
+    {
+      tag: "c:ingots/copper"
+    },
+    {
+      tag:"c:ingots/iron"
+    },
+    {
+      "type": "neoforge:single",
+      "amount": 10,
+      "fluid": "minecraft:lava"
+    }
+  ],
+  results: [
+    {
+      id: "pointblank:gunmetal_ingot"
+    }
+  ]
+}).id("create:compacting/gunmetal_lava")
+//VPB枪械金属锭配方
+
+})
